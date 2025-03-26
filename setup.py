144a47e3a8a4617b68af5dc79ac92570e08d8e25
Siddhant Mishra <90679433+siddhantmishra-4098@users.noreply.github.com>
2025-03-26 16:43:23 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="statistical_tests", 
-    version="0.2.1", 
-    author="ABHISHEK MISHRA" "SIDDHANT MISHRA",
-    author_email="abhishekmishra0106@gmail.com" "mishrasiddhant911@gmail.com",
+    name="statistical_tests",
+    version="0.2.1",
+    author="ABHISHEK MISHRA, SIDDHANT MISHRA, ANIRUDH PARAMESWARAN",
+    author_email="abhishekmishra0106@gmail.com, mishrasiddhant911@gmail.com, anirudhparameswaran@gmail.com",
     description="This Package is an implementation of multiple Statistical Tests e.g. Z-TEST, T-TEST, ANOVA, CHI SQUARE.",
-    url="https://github.com/yourusername/my_project", 
+    url="https://github.com/yourusername/my_project",
     packages=find_packages(),
     install_requires=[
         "numpy",
