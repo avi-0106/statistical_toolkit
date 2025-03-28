52318c8f019a4e8167564a036bbabb0c240f96d5
Abhishek Mishra <126774946+avi-0106@users.noreply.github.com>
2025-03-28 19:29:21 +0530

    Update setup.py : Version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="statistical_tests",
-    version="0.2.1",
+    version="0.3.0",
     author="ABHISHEK MISHRA, SIDDHANT MISHRA, ANIRUDH PARAMESWARAN",
     author_email="abhishekmishra0106@gmail.com, mishrasiddhant911@gmail.com, anirudhparameswaran@gmail.com",
     description="This Package is an implementation of multiple Statistical Tests e.g. Z-TEST, T-TEST, ANOVA, CHI SQUARE.",
